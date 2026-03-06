4b34880aebc410975393f24df53f3ccbaebb583d
angelorapana <angelorapana@icloud.com>
2026-03-06 11:17:33 +0100

    Aggiunto evento PreventDefault al form

--- post-form/src/App.jsx
+++ post-form/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-
+import { useState } from "react"
 
 function App() {
+
   const [formData, setFormData] = useState({
     author: "",
     title: "",
@@ -9,28 +9,59 @@ function App() {
     public: false
   })
 
+  const aggiornamento = (e) => {
+    const name = e.target.name
+    const value = e.target.type === "checkbox"
+      ? e.target.checked
+      : e.target.value
+
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const preventDefaultSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
   return (
-    <form>
+    <form onSubmit={preventDefaultSubmit}>
       <input
         type="text"
         name="author"
+        placeholder="Author"
+        value={formData.author}
+        onChange={aggiornamento}
       />
 
       <input
         type="text"
         name="title"
-      />
-
-      <textarea
-        name="body"
+        placeholder="Title"
+        value={formData.title}
+        onChange={aggiornamento}
       />
 
       <input
-        type="checkbox"
-        name="public"
+        name="body"
+        placeholder="Body"
+        value={formData.body}
+        onChange={aggiornamento}
       />
 
+      <label>
+        Pubblico:
+        <input
+          type="checkbox"
+          name="public"
+          checked={formData.public}
+          onChange={aggiornamento}
+        />
+      </label>
+
       <button type="submit">Invia</button>
+
     </form>
   )
 }
